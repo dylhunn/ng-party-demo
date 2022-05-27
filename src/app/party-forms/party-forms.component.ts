6972c23afe4951134ab16881fd8452486df7f79a
Dylan Hunn <dylhunn@gmail.com>
2022-05-27 15:27:32 -0700

    Update to rc.2

--- src/app/party-forms/party-forms.component.ts
+++ src/app/party-forms/party-forms.component.ts
@@ -4,26 +4,21 @@ import { FormArray, FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-party-forms',
   templateUrl: './party-forms.component.html',
-  styleUrls: ['./party-forms.component.css']
+  styleUrls: ['./party-forms.component.css'],
 })
 export class PartyFormsComponent implements OnInit {
-
   public party = new FormGroup({
     address: new FormGroup({
-      house: new FormControl(123, {initialValueIsDefault: true}),
-      street: new FormControl('Powell St', {initialValueIsDefault: true}),
+      house: new FormControl(123, { nonNullable: true }),
+      street: new FormControl('Powell St', { nonNullable: true }),
     }),
     formal: new FormControl(true),
-    foodOptions: new FormArray([
-      new FormControl('Soup'),
-    ])
+    foodOptions: new FormArray([new FormControl('Soup')]),
   });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   addMenuItem() {
     this.party.controls.foodOptions.push(new FormControl(''));
